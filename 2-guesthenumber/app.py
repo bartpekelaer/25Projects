81b85c26eb653d08235c203c2d44576beb31aac6
Bart Pekelaer <81241027+bartpekelaer@users.noreply.github.com>
2022-12-28 23:41:24 +0100

    computer guess added

--- 2-guesthenumber/app.py
+++ 2-guesthenumber/app.py
@@ -13,5 +13,26 @@ def guess(x):
     print(f"Correct! the number was {random_number}")
 
 
+def computer_guess(x):
+    low = 1
+    high = x
+    feedback = ""
+    while feedback != "c":
+        if low != high:
+            guess = random.randint(low, high)
+        else:
+            guess = low
+        guess = random.randint(low, high)
+        feedback = input(
+            f"Is {guess} too high (H), too low (L) or correct (C)"
+        ).lower()
+        if feedback == "h":
+            high = guess - 1
+        if feedback == "l":
+            low = guess + 1
+    print(f"Correct! The computer guessed {guess} correctly.")
+
+
 if __name__ == "__main__":
-    guess(20)
+    computer_guess(20)
+    # guess(20)
