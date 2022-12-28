7df086f8fc766198eb7713dfb3e99b12800efc17
Bart Pekelaer <81241027+bartpekelaer@users.noreply.github.com>
2022-12-28 23:20:55 +0100

    Madlibs

---
+++ 1-madlibs/app.py
@@ -0,0 +1,9 @@
+adj = input("Adjective: ")
+verb1 = input("Verbs: ")
+verb2 = input("Verbs: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so exited all the time because \
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}"
+
+print(madlib)
